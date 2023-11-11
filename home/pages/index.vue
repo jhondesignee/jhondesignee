c6f9eaadf408e0a29c97f9a254893be4df177cd3
jhondesignee <jhondesignee@gmail.com>
2023-11-11 02:01:11 -0300

    fix: fix title template

--- home/pages/index.vue
+++ home/pages/index.vue
@@ -2,17 +2,6 @@
   <ProjectCard />
 </template>
 <script lang="ts" setup>
-  useHead({
-    titleTemplate: "%s ¦ Jhon Designee",
-    htmlAttrs: { lang: "pt-br" },
-    link: [
-      {
-        rel: "icon",
-        type: "image/png",
-        href: "https://raw.githubusercontent.com/jhondesignee/jhondesignee/61c714de4a34f418f880117c705a22c125b30cc2/logo.png"
-      }
-    ]
-  })
   useSeoMeta({
     title: "Home",
     ogTitle: "Home",
@@ -24,5 +13,16 @@
     twitterImage: "https://raw.githubusercontent.com/jhondesignee/jhondesignee/61c714de4a34f418f880117c705a22c125b30cc2/logo.png",
     twitterCard: "summary_large_image",
     ogUrl: "https://jhondesignee.netlify.app"
+  })
+  useHead({
+    titleTemplate: "%s ¦ Jhon Designee",
+    htmlAttrs: { lang: "pt-br" },
+    link: [
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "https://raw.githubusercontent.com/jhondesignee/jhondesignee/61c714de4a34f418f880117c705a22c125b30cc2/logo.png"
+      }
+    ]
   })
 </script>
